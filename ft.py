d182139e48962b53b912061c02180d8bf4883701
devvratmiglani <devvratmiglani@gmail.com>
2024-03-06 23:42:00 +0530

    Added files

---
+++ ft.py
@@ -0,0 +1,27 @@
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import ThreadedFTPServer
+from FindMyIP import internal
+from colorama import Fore
+
+# Set up an authorizer with a user and password
+authorizer = DummyAuthorizer()
+authorizer.add_user("username", "password", ".", perm="elradfmw")
+authorizer.add_user("vlc", "password", ".", perm="elradfmw")
+authorizer.add_anonymous(".")
+
+# Instantiate the FTP handler with the given authorizer
+handler = FTPHandler
+handler.passive_ports = range(60000, 65535)
+handler.authorizer = authorizer
+handler.recv_buffer_size = 4096  # Adjust buffer size as needed
+handler.send_buffer_size = 4096  # Adjust buffer size as needed
+
+
+# Create and start the FTP server
+hostip = internal()
+server = ThreadedFTPServer((hostip, 2121), handler)
+print(f"{Fore.GREEN}[+] FTP server running at ftp://username:password@{hostip}:2121{Fore.RESET}")
+server.max_cons = 256
+server.max_cons_per_ip = 20
+server.serve_forever()
